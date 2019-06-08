739ae0f8147e3b0c794472770c07869aa0557c10
lrrinaudo <lrrinaudo@gmail.com>
2019-06-08 17:22:31 -0300

    Create POST user Closes #9.

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,15 +2,41 @@ import { Router } from 'express'
 
 const router = Router()
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 router.get('/usuarios', async (req, res) => {
   const users = await req.context.models.User.find()
   return res.send(users)
 })
 
-router.get('/usuarios/:userId/fabricas', async (req, res) => {
-    const users = await req.context.models.User.findById(req.params.userId)
-    return res.send(users['factories'])
+router.get('/usuarios/:userName/fabricas', async (req, res) => {
+    const users = await req.context.models.User.findOne({username: req.params.userName})
+    console.log(users);
+    if (users) {
+      return res.send(users['factories'])
+    }
+    else {
+      return res.status(404).send('Not found');
+    }
 })
+
+router.post('/usuarios/:userName', async (req, res) => {
+  const user = {
+    username: req.params.userName,
+    factories: []
+  }
+  req.context.models.User.create(user)
+  return res.send(user)
+})
+
+
 
 
 export default router
